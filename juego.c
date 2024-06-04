cca4872e751fa68cbc6f08fe7f99d1c48a94f267
María Emilia Ramírez <mariaemiliaramirezgomez@comunidad.unam.mx>
2024-06-04 01:06:33 +0000

    Se agregó la estructura básica del programa, falta implementar la lógica de las funciones

--- juego.c
+++ juego.c
@@ -10,6 +10,48 @@ typedef struct {
     char respuestaCorrecta;
 } Pregunta;
 
+// TODO: Dividir y asignar tareas para completar la implementación de las siguientes funciones: 
+
+/**
+ * Imprime la pregunta y sus opciones en la consola.
+ * 
+ * @param pregunta - La estructura de tipo Pregunta que contiene el texto de la pregunta y las opciones.
+ * @return No devuelve ningún valor.
+ */
+
+void imprimirPregunta(Pregunta pregunta) {
+    //AGREGAR LA IMPLEMENTACIÓN PARA IMPRIMIR EN CONSOLA LA PREGUNTA Y LAS OPCIONES.
+    //NO MODIFIQUES LA FIRMA DE LA FUNCIÓN.
+}
+
+/**
+ * Captura la respuesta del usuario desde la entrada estándar.
+ * 
+ * @return char - El carácter que representa la respuesta del usuario.
+ */
+
+char capturarRespuesta() {
+    //AGREGAR LA IMPLEMENTACIÓN PARA LEER LA RESPUESTA DEL JUGADOR.
+    //NO MODIFIQUES LA FIRMA DE LA FUNCIÓN.
+}
+
+
+/**
+ * Verifica si la respuesta del usuario es correcta. 
+ * Imprime un mensaje en consola indicando si ha respondido correctamente
+ * 
+ * @param pregunta La estructura de tipo Pregunta que contiene la pregunta y la respuesta correcta.
+ * @param respuesta La respuesta ingresada por el usuario.
+ * @return int - 1 si la respuesta es correcta, 0 si es incorrecta.
+ */
+
+int verificarRespuesta(Pregunta pregunta, char respuesta) {
+    //AGREGAR LA IMPLEMENTACIÓN PARA VERIFICAR SI RESPONDIÓ BIEN LA PREGUNTA.
+    //NO MODIFIQUES LA FIRMA DE LA FUNCIÓN. 
+}
+
+
+// NO MODIFICAR EL RESTO DEL CÓDIGO
 
 int main() {
     int puntaje = 0;
@@ -41,10 +83,20 @@ int main() {
 
     fclose(archivo); 
 
+    // Funcionamiento del juego
+
+    for (int i = 0; i < totalPreguntas; i++){
+        pregunta = preguntas[i];
+        imprimirPregunta(pregunta); 
+        respuesta = capturarRespuesta();
+        if (verificarRespuesta(pregunta, respuesta)){
+            puntaje ++; 
+        }
+            
+    }
+
     printf("\nTu puntaje total es: %i de %i\n", puntaje, totalPreguntas);
 
     return 0; 
 
 }
-
-
